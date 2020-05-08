2cf001ad93854839e1a70b57240ef2806f00f023
Marcos Vital <marcos901.everis@gamil.com>
2020-05-08 09:18:25 -0300

    Atualizaçao driver

--- src/test/java/page/BasePage.java
+++ src/test/java/page/BasePage.java
@@ -1,11 +1,25 @@
 package page;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Date;
+
+import javax.print.attribute.standard.PrinterURI;
+
+import org.hamcrest.Condition.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.io.Files;
 
 import maps.Maps;
 
@@ -23,7 +37,7 @@ public class BasePage {
   Maps maps = new Maps();
   
   public void navegar(String url) {
-    driver.navigate().to(url);
+    driver.get(url);
   }
   
   public void click(By element) {
@@ -45,9 +59,20 @@ public class BasePage {
   
   public void clicarItem(String campo) {
     click(maps.createLinkByText(campo));
+    takeScreenshot("clicar", driver);
   }
   
   public void botao(String campo) {
     click(maps.createSpanByText(campo));
+  }
+  
+  public static void takeScreenshot(String fileName, WebDriver driver) {
+    File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    Date data = new Date();
+    try {
+      FileHandler.copy(scrFile, new File(fileName + data.getTime() + ".png"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
--- src/test/java/scenarios/teste/BaseTest.java
+++ src/test/java/scenarios/teste/BaseTest.java
@@ -2,31 +2,25 @@ package scenarios.teste;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
-	public WebDriver driver;
-
-	@BeforeClass
-	public static void beforeClass() {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
-	}
-
-	@Before
-	public void before() {
-		driver = new ChromeDriver();
+  public WebDriver driver;
+  
+  @Before
+  public void before() {
+    System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
+    driver = new ChromeDriver();
     driver.manage().window().maximize();
-	}
-
-	@After
-	public void afterTest() {
-		try {
-			driver.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
+  }
+  
+  @After
+  public void afterTest() {
+    try {
+      driver.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
